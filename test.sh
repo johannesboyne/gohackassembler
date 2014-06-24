fdb9e2cf9be32858ab24a630e67af1a77240b51b
Johannes Boyne <johannes@boyne.de>
2014-06-24 18:41:55 +0200

    Refactor test.sh

--- test.sh
+++ test.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
-./gohack Pong.asm | diff Pong.hack -
 ./gohack Add.asm | diff Add.hack -
-./gohack Max.asm | diff Max.hack -
 ./gohack MaxL.asm | diff MaxL.hack -
+./gohack Max.asm | diff Max.hack -
 ./gohack Rect.asm | diff Rect.hack -
+./gohack Pong.asm | diff Pong.hack -
